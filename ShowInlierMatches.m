5d5dd6c6406444d640427f8d007f6ec2fe9009dc
RuibinMa <ruibinma@rma.cs.unc.edu>
2017-02-13 10:04:58 -0500

    used Dice coefficients

--- ShowInlierMatches.m
+++ ShowInlierMatches.m
@@ -1,17 +1,19 @@
-clear all; close all; clc;
-% use the following system command to fetch data from database file
-system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT rows FROM inlier_matches" > group1_result.csv');
+clear; close all; clc;
+%% use the following system command to fetch data from database file
+% the system command ">!", which means overwrite redirection, can be
+% different across different systems. Some system uses ">" for overwrite.
+system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT rows FROM inlier_matches" >! group1_result.csv');
 inlier_info = importdata('./group1_result.csv');
 inlier = inlier_info.data;
-system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT image_id FROM images" > group1_images.csv');
+system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT image_id FROM images" >! group1_images.csv');
 images_info = importdata('./group1_images.csv');
 image_id = images_info.data;
-system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT rows FROM keypoints" > group1_keypoints.csv');
+system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT rows FROM keypoints" >! group1_keypoints.csv');
 keypoints_info = importdata('./group1_keypoints.csv');
 keypoints = keypoints_info.data;
-
+%% read data from structures
 % in the database.db from colmap, the inlier_matches is laid in the
-% following pattern, where the x indicates the number is not contained in
+% following pattern, where the x indicates that the number is not contained in
 % the 'rows' variable. The numbers show the order. 
 %     x 1 2 3 4 5 6 7 8
 %     x x 9101112131415 
@@ -22,8 +24,6 @@ keypoints = keypoints_info.data;
 %     x x x x x x x3435
 %     x x x x x x x x36
 %     x x x x x x x x x
-
-%% read data from structures
 Num_Images = length(image_id);
 Num_Inlier = length(inlier);
 Matches = zeros(Num_Images);
@@ -31,10 +31,10 @@ id = 0;
 for i=1:Num_Images
     for j= i+1:Num_Images
         id = id+1;
-        Matches(i,j) = inlier(id)/(keypoints(i) + keypoints(j));
-        Matches(j,i) = inlier(id)/(keypoints(i) + keypoints(j));
+        Matches(i,j) = 2 * inlier(id)/(keypoints(i) + keypoints(j));
+        Matches(j,i) = 2 * inlier(id)/(keypoints(i) + keypoints(j));
         
     end
 end
-figure;
+figure('Name', 'Dice Coefficients');
 mesh(Matches);
