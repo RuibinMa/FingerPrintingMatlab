e2c9bde9b5a27fed649cf0383e65ebe45178a3e3
RuibinMa <ruibinma@rma.cs.unc.edu>
2017-02-14 14:49:42 -0500

    now can successfully extract data from database

--- ShowInlierMatches.m
+++ ShowInlierMatches.m
@@ -1,40 +1,29 @@
-clear; close all; clc;
+clear all; close all; clc;
 %% use the following system command to fetch data from database file
 % the system command ">!", which means overwrite redirection, can be
 % different across different systems. Some system uses ">" for overwrite.
-system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT rows FROM inlier_matches" >! group1_result.csv');
-inlier_info = importdata('./group1_result.csv');
+folder = 'group1';
+system(['sqlite3 -csv -header ../',folder,'/database.db "SELECT rows FROM inlier_matches" >! ',folder,'_result.csv']);
+inlier_info = importdata(['./',folder,'_result.csv']);
 inlier = inlier_info.data;
-system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT image_id FROM images" >! group1_images.csv');
-images_info = importdata('./group1_images.csv');
+system(['sqlite3 -csv -header ../',folder,'/database.db "SELECT pair_id FROM inlier_matches" >! ',folder,'_pairid.csv']);
+pairid_info = importdata(['./',folder,'_pairid.csv']);
+pairid = pairid_info.data;
+system(['sqlite3 -csv -header ../',folder,'/database.db "SELECT image_id FROM images" >! ',folder,'_images.csv']);
+images_info = importdata(['./',folder,'_images.csv']);
 image_id = images_info.data;
-system('sqlite3 -csv -header ../group1withgarbage/database.db "SELECT rows FROM keypoints" >! group1_keypoints.csv');
-keypoints_info = importdata('./group1_keypoints.csv');
+system(['sqlite3 -csv -header ../',folder,'/database.db "SELECT rows FROM keypoints" >! ',folder,'_keypoints.csv']);
+keypoints_info = importdata(['./',folder,'_keypoints.csv']);
 keypoints = keypoints_info.data;
 %% read data from structures
-% in the database.db from colmap, the inlier_matches is laid in the
-% following pattern, where the x indicates that the number is not contained in
-% the 'rows' variable. The numbers show the order. 
-%     x 1 2 3 4 5 6 7 8
-%     x x 9101112131415 
-%     x x x161718192021
-%     x x x x2223242526
-%     x x x x x27282930
-%     x x x x x x313233
-%     x x x x x x x3435
-%     x x x x x x x x36
-%     x x x x x x x x x
 Num_Images = length(image_id);
 Num_Inlier = length(inlier);
 Matches = zeros(Num_Images);
-id = 0;
-for i=1:Num_Images
-    for j= i+1:Num_Images
-        id = id+1;
-        Matches(i,j) = 2 * inlier(id)/(keypoints(i) + keypoints(j));
-        Matches(j,i) = 2 * inlier(id)/(keypoints(i) + keypoints(j));
-        
-    end
+for i=1:length(pairid)
+    id2 = mod(pairid(i), 2147483647);
+    id1 = round((pairid(i) - id2)/2147483647)+1;
+    Matches(id1, id2) = 2*inlier(i)/(keypoints(id1) + keypoints(id2));
+    Matches(id2, id1) = 2*inlier(i)/(keypoints(id1) + keypoints(id2));
 end
 figure('Name', 'Dice Coefficients');
-mesh(Matches);
+imshow(mat2gray(Matches));
