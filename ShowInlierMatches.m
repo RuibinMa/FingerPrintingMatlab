e1ab5e83815fd2b3407aec047b3e56c9fa72f947
RuibinMa <ruibinma@cs.unc.edu>
2017-02-16 16:02:32 -0500

    got the result from different num_images

--- ShowInlierMatches.m
+++ ShowInlierMatches.m
@@ -1,6 +1,7 @@
 clear all; close all; clc;
 %% choice
-folder = 'group3';
+database_path = '/playpen/matchingResults/';
+database_name = 'vocab1048576_50';
 compareWithExhaustiveMatching = 1;
 %% use the following system command to fetch data from database file
 % the system command ">!", which means overwrite redirection, can be
@@ -8,17 +9,21 @@ compareWithExhaustiveMatching = 1;
 if(~exist('./cache', 'dir'))
     mkdir('./cache');
 end
-system(['sqlite3 -csv -header ../',folder,'/database.db "SELECT rows FROM inlier_matches" > ./cache/',folder,'_result.csv']);
-inlier_info = importdata(['./cache/',folder,'_result.csv']);
+delete('./cache/*');
+if(~exist('./output', 'dir'))
+    mkdir('./output');
+end
+system(['sqlite3 -csv -header ',database_path, database_name, '.db "SELECT rows FROM inlier_matches" > ./cache/result.csv']);
+inlier_info = importdata(['./cache/result.csv']);
 inlier = inlier_info.data;
-system(['sqlite3 -csv -header ../',folder,'/database.db "SELECT pair_id FROM inlier_matches" > ./cache/',folder,'_pairid.csv']);
-pairid_info = importdata(['./cache/',folder,'_pairid.csv']);
+system(['sqlite3 -csv -header ',database_path, database_name, '.db "SELECT pair_id FROM inlier_matches" > ./cache/pairid.csv']);
+pairid_info = importdata(['./cache/pairid.csv']);
 pairid = pairid_info.data;
-system(['sqlite3 -csv -header ../',folder,'/database.db "SELECT image_id FROM images" > ./cache/',folder,'_images.csv']);
-images_info = importdata(['./cache/',folder,'_images.csv']);
+system(['sqlite3 -csv -header ',database_path, database_name, '.db "SELECT image_id FROM images" > ./cache/images.csv']);
+images_info = importdata(['./cache/images.csv']);
 image_id = images_info.data;
-system(['sqlite3 -csv -header ../',folder,'/database.db "SELECT rows FROM keypoints" > ./cache/',folder,'_keypoints.csv']);
-keypoints_info = importdata(['./cache/',folder,'_keypoints.csv']);
+system(['sqlite3 -csv -header ',database_path, database_name, '.db "SELECT rows FROM keypoints" > ./cache/keypoints.csv']);
+keypoints_info = importdata(['./cache/keypoints.csv']);
 keypoints = keypoints_info.data;
 %% read data from structures
 Num_Images = length(image_id);
@@ -27,30 +32,45 @@ Num_Inlier = length(inlier);
 Matches = zeros(Num_Images);
 for i=1:length(pairid)
     id2 = mod(pairid(i), 2147483647);
-    id1 = round((pairid(i) - id2)/2147483647)+1;
-    Matches(id1, id2) = 2*inlier(i)/(keypoints(id1) + keypoints(id2));
-    Matches(id2, id1) = 2*inlier(i)/(keypoints(id1) + keypoints(id2));
+    id1 = floor((pairid(i) - id2)/2147483647 + 0.5);
+    Matches(id1, id2) = inlier(i);%/(keypoints(id1) + keypoints(id2));
+    Matches(id2, id1) = inlier(i);%/(keypoints(id1) + keypoints(id2));
 end
-figure('Name', 'Dice Coefficients');
-imshow(mat2gray(Matches));
+%figure('Name', 'Dice Coefficients');
+%imshow(mat2gray(Matches));
 %% compare VocabTree Matching with Exhaustive matching
 if(compareWithExhaustiveMatching)
-    system(['sqlite3 -csv -header ../',folder,'/exhaustivematching.db "SELECT rows FROM inlier_matches" >! ./cache/',folder,'_exhaustiveresult.csv']);
-    exhaustiveinlier_info = importdata(['./cache/',folder,'_exhaustiveresult.csv']);
+    exhaustivedb_name = 'exhaustivematching';
+    system(['sqlite3 -csv -header ',database_path, exhaustivedb_name, '.db "SELECT rows FROM inlier_matches" >! ./cache/exhaustiveresult.csv']);
+    exhaustiveinlier_info = importdata(['./cache/exhaustiveresult.csv']);
     exhaustiveinlier = exhaustiveinlier_info.data;
-    system(['sqlite3 -csv -header ../',folder,'/exhaustivematching.db "SELECT pair_id FROM inlier_matches" >! ./cache/',folder,'_exhaustivepairid.csv']);
-    exhaustivepairid_info = importdata(['./cache/',folder,'_exhaustivepairid.csv']);
+    system(['sqlite3 -csv -header ',database_path, exhaustivedb_name, '.db "SELECT pair_id FROM inlier_matches" >! ./cache/exhaustivepairid.csv']);
+    exhaustivepairid_info = importdata(['./cache/exhaustivepairid.csv']);
     exhaustivepairid = exhaustivepairid_info.data;
     % exhaustive matching
     ExhaustiveMatches = zeros(Num_Images);
     for i=1:length(exhaustivepairid)
         id2 = mod(exhaustivepairid(i), 2147483647);
-        id1 = round((exhaustivepairid(i) - id2)/2147483647)+1;
-        ExhaustiveMatches(id1, id2) = 2*exhaustiveinlier(i)/(keypoints(id1) + keypoints(id2));
-        ExhaustiveMatches(id2, id1) = 2*exhaustiveinlier(i)/(keypoints(id1) + keypoints(id2));
+        id1 = floor((exhaustivepairid(i) - id2)/2147483647 + 0.5);
+        ExhaustiveMatches(id1, id2) = exhaustiveinlier(i);%/(keypoints(id1) + keypoints(id2));
+        ExhaustiveMatches(id2, id1) = exhaustiveinlier(i);%/(keypoints(id1) + keypoints(id2));
     end
-    retriveRatio = sum(inlier) / sum(exhaustiveinlier);
-    fprintf('Retrive Ratio = %.2f%%\n', retriveRatio*100);
+    ax = (1:1:Num_Images);
+    matchedImages = zeros(Num_Images, 1);
+    exhaustiveImages = zeros(Num_Images, 1);
+    for i=1:Num_Images
+        matchedImages(i) =  length(find(Matches(:, i)>0.5));
+        exhaustiveImages(i) = length(find(ExhaustiveMatches(:, i)>0.5));
+    end
+    retrivedRatio = sum(matchedImages) / sum(exhaustiveImages);
+    fprintf('Average Retrived Ratio = %.2f%%\n', retrivedRatio*100);
+    h = figure;
+    plot(ax, matchedImages, 'r');
+    hold on;
+    plot(ax, exhaustiveImages, 'b');
+    hold off;
+    title(sprintf('Average Retrived Ratio = %.2f%%\n', retrivedRatio*100));
+    saveas(h, ['./output/', database_name, '.jpg'], 'jpg');
 end
 %% delete cache
 delete('./cache/*');
