57b295e91c18d2ea9d67bdb6f9fef0ab66cb8a29
ruibinma <ruibinma@cs.unc.edu>
2017-03-29 21:16:42 -0400

    prepare included the unregistered pairs

--- PrepareDatasetForJudgement.m
+++ PrepareDatasetForJudgement.m
@@ -16,37 +16,64 @@ for i=1:length(models)
                 break;
             end
         end
-        disp(name);
     end
 end
 
 listOfFrames = sort(listOfFrames);
-TempSelectedFarAwayPairs = zeros(size(FarAwayPairs));
+RegisteredSelectedFarAwayPairs = zeros(size(FarAwayPairs));
 pairid = 1;
 for i=1:size(FarAwayPairs, 1)
     id1 = FarAwayPairs(i, 1);
     id2 = FarAwayPairs(i, 2);
     n = FarAwayPairs(i, 3);
     if(~isempty(find(listOfFrames == id1, 1)) && ~isempty(find(listOfFrames == id2, 1)))
-        TempSelectedFarAwayPairs(pairid, :) = [id1, id2, n, 0];
+        RegisteredSelectedFarAwayPairs(pairid, :) = [id1, id2, n, 0];
         pairid = pairid + 1;
     end
 end
-TempSelectedFarAwayPairs = TempSelectedFarAwayPairs(1:pairid-1, :);
-SelectedFarAwayPairs = [];
+RegisteredSelectedFarAwayPairs = RegisteredSelectedFarAwayPairs(1:pairid-1, :);
+SelectedFarAwayPairs1 = [];
 for i=1:length(listOfFrames)
     id1 = listOfFrames(i);
-    pos = find(TempSelectedFarAwayPairs(:,1) == id1);
-    id2s = TempSelectedFarAwayPairs(pos,2);
+    pos = find(RegisteredSelectedFarAwayPairs(:,1) == id1);
+    id2s = RegisteredSelectedFarAwayPairs(pos,2);
     if(~isempty(id2s))
         [~, idx] = max(abs(id2s - id1*ones(size(id2s))));
-        id2 = TempSelectedFarAwayPairs(pos(idx), 2);
-        SelectedFarAwayPairs = [SelectedFarAwayPairs; [id1, id2, TempSelectedFarAwayPairs(pos(idx), 3), 0]];
+        id2 = RegisteredSelectedFarAwayPairs(pos(idx), 2);
+        SelectedFarAwayPairs1 = [SelectedFarAwayPairs1; [id1, id2, RegisteredSelectedFarAwayPairs(pos(idx), 3), 0]];
     end
 end
 
-x = randsample(length(SelectedFarAwayPairs), 100);
-x = sort(x);
-SelectedFarAwayPairs = SelectedFarAwayPairs(x, :);
+UnRegisteredSelectedFarAwayPairs = zeros(size(FarAwayPairs));
+pairid = 1;
+for i=1:size(FarAwayPairs, 1)
+    id1 = FarAwayPairs(i, 1);
+    id2 = FarAwayPairs(i, 2);
+    n = FarAwayPairs(i, 3);
+    if(isempty(find(listOfFrames == id1, 1)) || isempty(find(listOfFrames == id2, 1)))
+        UnRegisteredSelectedFarAwayPairs(pairid, :) = [id1, id2, n, 0];
+        pairid = pairid + 1;
+    end
+end
+SelectedFarAwayPairs2 = [];
+UnRegisteredSelectedFarAwayPairs = UnRegisteredSelectedFarAwayPairs(1:pairid-1, :);
+for i=1:Num_Images
+    id1 = i;
+    pos = find(UnRegisteredSelectedFarAwayPairs(:,1) == id1);
+    id2s = UnRegisteredSelectedFarAwayPairs(pos,2);
+    if(~isempty(id2s))
+        [~, idx] = max(abs(id2s - id1*ones(size(id2s))));
+        id2 = UnRegisteredSelectedFarAwayPairs(pos(idx), 2);
+        SelectedFarAwayPairs2 = [SelectedFarAwayPairs2; [id1, id2, UnRegisteredSelectedFarAwayPairs(pos(idx), 3), 0]];
+    end
+end
+
+
+
+x1 = randsample(length(SelectedFarAwayPairs1), 100);
+x1 = sort(x1);
+x2 = randsample(length(SelectedFarAwayPairs2), 100);
+x2 = sort(x2);
+SelectedFarAwayPairs = [SelectedFarAwayPairs1(x1, :); SelectedFarAwayPairs2(x2, :)];
 
 save('SelectedFarAwayPairs.mat', 'SelectedFarAwayPairs');
